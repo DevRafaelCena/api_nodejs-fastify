05f114a8c92b68500fe3a8f14e7473da6430dd89
Rafael Cena <rafael.cena@hotmail.com>
2023-02-12 09:45:30 -0300

    cookies

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -42,10 +42,21 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body,
     )
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = crypto.randomBytes(16).toString('hex')
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
     await knex('transactions').insert({
       id: crypto.randomBytes(16).toString('hex'),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return reply.status(201).send()
