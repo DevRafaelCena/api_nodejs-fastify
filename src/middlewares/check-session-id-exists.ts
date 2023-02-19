51ed5b1ccfcaaaf496d300973ff0a0d402a41595
Rafael Cena <rafael.cena@hotmail.com>
2023-02-19 12:23:00 -0300

    ajustes e implementação de testes

--- src/middlewares/check-session-id-exists.ts
+++ src/middlewares/check-session-id-exists.ts
@@ -1,12 +1,14 @@
-import { FastifyRequest, FastifyReply } from 'fastify'
+import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function checkSessionIdExists(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
-  const sessionID = request.cookies.sessionId
+  const sessionId = request.cookies.sessionId
 
-  if (!sessionID) {
-    return reply.status(401).send({ error: 'Unauthorized' })
+  if (!sessionId) {
+    return reply.status(401).send({
+      error: 'Unauthorized.',
+    })
   }
 }
